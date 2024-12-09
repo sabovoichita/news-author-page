49a2a6c4b5f7d5a7fbc16e2067e6b28ccd95d74d
Voichita Maria <sabo_voichita@yahoo.com>
2024-12-09 21:05:55 +0100

    24. Your fCC Authors Page is now complete. But you could improve on a few things.
    First, if you click the Load More Authors button a couple of times, you'll see that it won't add more authors to the page. That's because you've reached the end of the authors list. For a better user experience, you should make it clear when there's no more data to display by disabling the button and changing its text. An if statement is the perfect tool for this.
    
    Inside the fetchMoreAuthors function, write an if statement and set the condition to authorDataArr.length <= endingIndex – meaning there's no more data to load.

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ const fetchMoreAuthors = () => {
   startingIndex += 8;
   endingIndex += 8;
   displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
+  if (authorDataArr.length <= endingIndex) {
+  }
 };
 
 const displayAuthors = (authors) => {
