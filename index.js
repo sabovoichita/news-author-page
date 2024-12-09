67938a1e6ec5a7d220bcc0cf44195cccb926e085
Voichita Maria <sabo_voichita@yahoo.com>
2024-12-09 20:55:06 +0100

    18. The next thing you'll show are biographical details about the author. You can do this with bio that you destructured earlier.
    Add a paragraph element with the class "bio", then interpolate bio inside the paragraph element.

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const displayAuthors = (authors) => {
     <div id="${index}" class="user-card">
       <h2 class="author-name">${author}</h2>
     <img class="user-img" src="${image}" alt="${author} avatar">
+      <p class="bio">${bio}</p>
     </div>
     `;
   });
