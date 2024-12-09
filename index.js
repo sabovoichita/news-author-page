97501721fe6f55864eba81ef8a78ee9763fae6ee
Voichita Maria <sabo_voichita@yahoo.com>
2024-12-09 20:33:52 +0100

    9. Inside your displayAuthors function, chain .forEach() to authors.

--- index.js
+++ index.js
@@ -14,4 +14,6 @@ fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
     console.error(`There was an error: ${err}`);
   });
 
-const displayAuthors = (authors) => {};
+const displayAuthors = (authors) => {
+  authors.forEach();
+};
