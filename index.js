53dadd002f44433aae04dd71bc00dc2af4fd4a3f
Voichita Maria <sabo_voichita@yahoo.com>
2024-12-09 20:14:19 +0100

    1. All the HTML and CSS for this project has been provided for you. You can take a look at the two files to familiarize yourself with them.
    Start by getting the #author-container and #load-more-btn elements with the .getElementById() method. Assign them to the variables authorContainer and loadMoreBtn, respectively.
    
    The variables will not change, so use const to declare them.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("Welcome!");
+const authorContainer = document.getElementById("author-container");
+const loadMoreBtn = document.getElementById("load-more-btn");
+console.log("Welcome!", authorContainer);
+console.log("Welcome!", loadMoreBtn);
